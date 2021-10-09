6eec02f9f6e101b72d91f85094736112c211dab9
matt <github@mashton.com>
2021-10-09 20:24:15 +0100

    Changed to using string actions with a list of colours and a dictionary of values.
    Changed the logic to actually work.
    
    Implemented correct foul values.
    
    Have naive reflex agent, that pots reds and blacks after reds.

--- snooker.py
+++ snooker.py
@@ -2,76 +2,18 @@ from def_agents import Agent, RandomAgentProgram
 from def_agents import Environment
 from def_agents import Thing
 
+red, yellow, green, brown, blue, pink, black = "Red", "Yellow", "Green", "Brown", "Blue", "Pink", "Black"
 
-# Ball Classes
-class Ball(Thing):
-    def __eq__(self, other):
-        return self.value == other.value
+colours = [yellow, green, brown, blue, pink, black]
 
-    pass
-
-
-class Red(Ball):
-    def __init__(self):
-        self.value = 1
-
-    def __repr__(self):
-        return "Red"
-
-    def __str__(self):
-        return "Red"
-
-
-class Colour(Ball):
-    pass
-
-
-class Yellow(Colour):
-    def __init__(self):
-        self.value = 2
-
-    def __repr__(self):
-        return "Yellow"
-
-
-class Green(Colour):
-    def __init__(self):
-        self.value = 3
-
-    def __repr__(self):
-        return "Green"
-
-
-class Brown(Colour):
-    def __init__(self):
-        self.value = 4
-
-    def __repr__(self):
-        return "Brown"
-
-
-class Blue(Colour):
-    def __init__(self):
-        self.value = 5
-
-    def __repr__(self):
-        return "Blue"
-
-
-class Pink(Colour):
-    def __init__(self):
-        self.value = 6
-
-    def __repr__(self):
-        return "Pink"
-
-
-class Black(Colour):
-    def __init__(self):
-        self.value = 7
-
-    def __repr__(self):
-        return "Black"
+values = {red: 1,
+          yellow: 2,
+          green: 3,
+          brown: 4,
+          blue: 5,
+          pink: 6,
+          black: 7
+          }
 
 
 class TrivialSnookerEnvironment(Environment):
@@ -87,16 +29,10 @@ class TrivialSnookerEnvironment(Environment):
         self.status = {'balls remaining': [],
                        'balls potted': []}
 
-        for i in range(1):
-            self.status['balls remaining'].append(Red())
-        for ball in [Yellow(), Green(), Brown(), Blue(), Pink(), Black()]:
-            self.status['balls remaining'].append(ball)
-
-    def run(self):
-        super().run(1000)
-
-    def thing_classes(self):
-        return [Ball]
+        for i in range(15):
+            self.ballsRemaining().append(red)
+        for c in colours:
+            self.ballsRemaining().append(c)
 
     def percept(self, agent):
         """Returns the status of balls remaining and potted """
@@ -110,57 +46,52 @@ class TrivialSnookerEnvironment(Environment):
 
     def execute_action(self, agent, action):
         """Change balls potted and remaining status; track performance.
-        Score ball value for each correct ball potted; -4 for each invalid one."""
+        Score ball value for each correct ball potted; -4, -5, -6 or -7
+        depending on ball value for each invalid one."""
         if action is not "NoOp":
-            matchingBalls = [b for b in self.ballsRemaining() if b == action]
-            ballOn = self.ballOn()
-            if ballOn is None:
-                agent.performance -= 4
-            elif isinstance(action, ballOn):
-                agent.performance += action.value
+            ballsOn = self.ballsOn()
+            if ballsOn is None:
+                agent.performance -= max(4, values[action])
+            elif action in ballsOn:
+                agent.performance += values[action]
                 self.pot(action)
             else:
-                agent.performance -= 4
+                agent.performance -= max(4, values[action])
 
     def pot(self, ball):
-
-        if isinstance(ball, Red):
+        if ball == red:
             self.ballsRemaining().remove(ball)
-
-        if issubclass(type(ball), Colour) and not isinstance(self.lowestValueBallRemaining(), Red):
+        if ball in colours and self.lowestValueBallRemaining() != red:
             self.ballsRemaining().remove(ball)
-
         self.ballsPotted().append(ball)
 
-    def ballOn(self):
+    def ballsOn(self):
         if len(self.ballsRemaining()) == 0:
             return None
         elif len(self.ballsPotted()) == 0:
-            return Red
-        else:
-            lastBallPotted = self.ballsPotted()[-1]
+            return [red]
 
-        if isinstance(lastBallPotted, Red) and isinstance(self.lowestValueBallRemaining(), Red):
-            return Colour
-        elif isinstance(lastBallPotted, Colour):
-            return Red
+        lastBallPotted = self.ballsPotted()[-1]
+
+        if lastBallPotted == red:
+            return colours
+        elif lastBallPotted in colours and self.lowestValueBallRemaining() == red:
+            return [red]
         else:
-            return self.lowestValueBallRemaining()
+            return [self.lowestValueBallRemaining()]
 
     def lowestValueBallRemaining(self):
-        sortedBallsRemaining = sorted(self.ballsRemaining(), key=lambda x: x.value)
+        sortedBallsRemaining = sorted(self.ballsRemaining(), key=lambda x: values[x])
         lowestValueBallRemaining = sortedBallsRemaining[0]
         return lowestValueBallRemaining
 
 
 def RandomSnookerAgent():
     """Randomly choose a ball colour to pot."""
-    return Agent(RandomAgentProgram([Red(), Yellow(), Green(), Brown(), Blue(), Pink(), Black(), "NoOp"]))
+    return Agent(RandomAgentProgram([red, yellow, green, brown, blue, pink, black, "NoOp"]))
 
 
-def run_RandomSnookerAgent():
-    # create an object of the RandomVacuumAgent
-    agent = RandomSnookerAgent()
+def run_SnookerAgent(agent):
     # create an object of TrivialVacuumEnvironment
     environment = TrivialSnookerEnvironment()
     # add agent to the environment
@@ -171,5 +102,20 @@ def run_RandomSnookerAgent():
     print(f'Performance: {agent.performance}')
 
 
+def ReflexSnookerAgent():
+    """A reflex agent for the trivial snooker environment. [Figure 2.8]"""
+    def program(percept):
+        potted = percept['balls potted']
+        if not potted:
+            action = red
+        elif potted[-1] == red:
+            action = black
+        else:
+            action = red
+        print(percept, action)
+        return action
+    return Agent(program)
+
+
 if __name__ == '__main__':
-    run_RandomSnookerAgent()
+    run_SnookerAgent(ReflexSnookerAgent())
